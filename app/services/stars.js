9e536dd09b69e5d143e846a4f7aa808513db0b7b
AlexexDenimus <demin.leha1998@yandex.ru>
2020-02-28 22:50:21 +0300

    first data

---
+++ app/services/stars.js
@@ -0,0 +1,13 @@
+export const stars = {
+  0: "Victoria June",
+  1: "Lana Rhodes",
+  2: "Karma RX",
+  3: "Elsa Jean",
+  4: "Jessa Rhodes",
+  5: "Riley Reid",
+  6: "Abella Danger",
+  7: "Liya Silver",
+  8: "Nicole Aniston",
+  9: "Mia Khalifa",
+  10: "Brandi Love"
+};
